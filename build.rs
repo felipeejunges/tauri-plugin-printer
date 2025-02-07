165fbfa4886f5ae6d4978c474eb9b0d8b575f1c4
Felipe Junges <felipeejunges@hotmail.com>
2025-02-07 14:06:57 -0300

    permissions

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-const COMMANDS: &[&str] = &["create_temp_file", "remove_temp_file", "get_printers", "get_printers_by_name", "print_pdf", "get_jobs", "get_jobs_by_id", "resume_job", "restart_job", "pause_job", "remove_job"];
+const COMMANDS: &[&str] = &["create_temp_file", "remove_temp_file", "get_printers", "get_printers_by_name", "print_pdf", "custom_get_printers_by_name", "custom_print_pdf", "get_jobs", "get_jobs_by_id", "resume_job", "restart_job", "pause_job", "remove_job"];
 
 fn main() {
   tauri_plugin::Builder::new(COMMANDS)
